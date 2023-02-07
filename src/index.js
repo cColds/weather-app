0543addf7fd485948387043a4de811308904c6a5
cColds <103373668+cColds@users.noreply.github.com>
2023-02-06 22:36:43 -0600

    Add weather info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("jif");
+fetch(
+	"https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d92ae7934e5fdcdcd8e453a8b3bbadd7"
+)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+
+const person = {
+	name: "Harmeet Matharoo",
+	age: 23,
+	followers: [
+		{
+			name: "Thaddaeus Fam",
+			username: "cColds",
+		},
+	],
+};
+
+if (person.followers.some((follower) => follower.username === "cColds")) {
+	console.log("New Follower: cColds");
+}
+
+const cColds = person.followers[0];
+
+if (cColds.isBase) {
+	console.log("base");
+} else {
+	console.log("!base");
+}
