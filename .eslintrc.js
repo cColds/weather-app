11fdcf6d1cda4137faadbd43f5727a7b8b4a8c20
cColds <103373668+cColds@users.noreply.github.com>
2023-02-12 19:51:00 -0600

    Update weather forecast on search

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,5 @@ module.exports = {
 		ecmaVersion: "latest",
 		sourceType: "module",
 	},
-	rules: {},
+	rules: { "no-param-reassign": ["error", { props: false }] },
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import weatherIcons from "./weatherIcons.json";
 
-console.log(weatherIcons);
 const searchBar = document.querySelector(".search-bar");
 
 // main weather
@@ -25,8 +24,18 @@ const sunrise = document.querySelector(".sunrise-value");
 const sunset = document.querySelector(".sunset-value");
 const apiKey = "d92ae7934e5fdcdcd8e453a8b3bbadd7";
 
+// forecast
+
+const weatherForecastDay = document.querySelectorAll(".day-forecast-date");
+const weatherForecastDescription = document.querySelectorAll(
+	".day-forecast-weather-description"
+);
+
+const weatherForecastTemperature = document.querySelectorAll(
+	".day-forecast-temperature"
+);
+
 function checkWeatherType(weather) {
-	console.log(weather, weatherIcons.snow);
 	if (weather === "Clear") {
 		weatherIcon.src = weatherIcons.clear.day;
 	} else if (weather === "Clouds") {
@@ -42,17 +51,17 @@ function checkWeatherType(weather) {
 	}
 }
 
+const unit = "imperial"; // set metric option later
+
 // weather forecast
 async function updateWeatherInfo() {
 	try {
-		const searchBarLocation = searchBar.value;
-		const unit = "imperial"; // set metric option later
-
 		const data = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?q=${searchBarLocation}&units=${unit}&appid=${apiKey}`
+			`https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&units=${unit}&appid=${apiKey}`
 		);
 
 		const response = await data.json();
+
 		checkWeatherType(response.weather[0].main);
 		weatherDescription.textContent = response.weather[0].description;
 		temperature.textContent = response.main.temp.toFixed(0);
@@ -65,15 +74,47 @@ async function updateWeatherInfo() {
 		sunrise.textContent = response.sys.sunrise;
 		sunset.textContent = response.sys.sunset;
 		location.textContent = `${response.name}, ${response.sys.country}`;
-		console.log(location.textContent);
+	} catch {
+		console.error("Failed to resolve weather info");
+	}
+}
+
+async function updateWeatherForecast() {
+	try {
+		const weatherForecastData = await fetch(
+			`https://api.openweathermap.org/data/2.5/forecast?q=${searchBar.value}&units=${unit}&appid=${apiKey}`
+		);
+		const response = await weatherForecastData.json();
+		const dailyForecast = [0, 8, 16, 24, 32, 40]; // daily forecast for 5 days
+
+		// day
+		for (let i = 0; i < weatherForecastDay.length; i += 1) {
+			weatherForecastDay[i].textContent =
+				response.list[dailyForecast[i]].dt_txt;
+		}
+
+		// weather description
+		for (let i = 0; i < weatherForecastDescription.length; i += 1) {
+			weatherForecastDescription[i].textContent =
+				response.list[dailyForecast[i]].weather[0].description;
+		}
+
+		// temperature
+		for (let i = 0; i < weatherForecastTemperature.length; i += 1) {
+			weatherForecastTemperature[i].textContent = `${response.list[
+				dailyForecast[i]
+			].main.temp.toFixed(0)}°`;
+		}
+
 		console.log(response);
 	} catch {
-		console.log("no lol");
+		console.error("Failed to resolve weather forecast");
 	}
 }
 
 function searchWeatherInfo() {
 	updateWeatherInfo();
+	updateWeatherForecast();
 	searchBar.value = "";
 }
 
